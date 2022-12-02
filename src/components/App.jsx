ac6bf0ff5611e9e81c7f1646d79b5fa7f7d1dbf4
FILIPRAIDER <filipraider123@gmail.com>
2022-12-02 14:01:28 -0500

    cambios importantes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello React!!!</h1>;
+  return (
+    <>
+      <h1>Guia para usar webpack:</h1>
+      <a href="https://www.notion.so/Curso-de-Webpack-797d35d9af4a4c13b5ddfa3160e953ed">
+        Link directo
+      </a>
+    </>
+  );
 };
 
 export default App;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,23 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: "/",
     },
     resolve: {
         extensions: ['.js', '.jsx'],
-        asias: {
-            '@components': path.resolve(__dirname, 'src/components/'),
-            '@styles': path.resolve(__dirname, 'src/styles/')
-        }
     },
     mode: 'development',
     module: {
@@ -52,13 +44,12 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css'
         }),
-        new CleanWebpackPlugin(),
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3006
     }
 }
